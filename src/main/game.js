d28b6b425b4fb19171d3afee492ad40a162bba14
kxwzhang <kxwzhang@gmail.com>
2020-09-08 14:36:34 -0700

    Clear treats

--- src/main/game.js
+++ src/main/game.js
@@ -58,11 +58,61 @@ class Game {
         }
     }
 
+    // Initializes the level
     initLevel() {
         // clear the blocks
         // generate new blocks
         let message = 'LEVEL ' + this.level + '\nGET READY'
         // display message using this.displayMessage()
         // Add sounds
+    }
+
+    // Clears the blocks
+    clearBlocks() {
+        this.blocks = [];
+    }
+
+    // Clears the treats
+    clearTreats() {
+        paddle.shrink(); // shrink the paddle
+        paddle.stopBeam(); // stop the beam
+        this.balls.forEach(ball => {
+            if (ball.magnet) ball.demagnetize;
+            ball.launch();
+        })
+    }
+
+    // Updates the score
+    updateScore(block) {
+        let points = 0;
+        switch (block.type) {
+            case 2:
+                points = 50 * this.level;
+                break;
+            case 3:
+                points = 60;
+                break;
+            case 4:
+                points = 70;
+                break;
+            case 5:
+                points = 80;
+                break;
+            case 6:
+                points = 90;
+                break;
+            case 7:
+                points = 100;
+                break;
+            case 8:
+                points = 110;
+                break;
+            case 9:
+                points = 120;
+                break;
+            default:
+                break;
+        }
+        this.score += points; // update score with points
     }
 }
