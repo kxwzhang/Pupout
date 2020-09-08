c78d28a376d4cf242f3eb509c0bc64e1b5892eaa
kxwzhang <kxwzhang@gmail.com>
2020-09-08 14:04:41 -0700

    Lay out the basic game logic

--- src/main/game.js
+++ src/main/game.js
@@ -1,6 +1,3 @@
-// Game Variables
-let score = 0;
-
 // Canvas Setup
 function setup() {
     createCanvas(800, 700);
@@ -9,6 +6,54 @@ function setup() {
 function draw() {
     background('burlywood');
     ellipse(100, 100, 100, 100);
-    text(`Score: ${score}`, width - 100, 50);
+    // text(`Score: ${score}`, width - 100, 50);
     fill(255);
+}
+
+// Game Class
+class Game {
+    constructor() {
+        this.menu = true;
+        this.paused = false;
+        this.stopped = false;
+        // this.paddle = new Paddle(); // add new Paddle class
+        // this.balls = [new balls()]; // an array of balls with new ball 
+        this.treats = [];
+        this.blocks = [];
+        this.beams = [];
+        this.level = 1;
+        this.score = 0;
+        this.lives = 3;
+        this.numFrames = 0;
+    }
+
+    update() {
+        if (!this.menu && !this.paused && !this.stopped) {
+            // 1. should first update the paddle
+
+            // 2. iterate through the balls and update them
+
+            // 3. iterate through the treats and update them
+
+            // 4. iterate through the beams and update them
+
+            // Check if not on the menu, then check if all the blocks have been cleared
+            // If all blocks cleared then initialize next level
+            // Increment the numFrames
+        }
+    }
+
+    show() {
+        if (!this.stopped) {
+            background('lightblue');
+            // Draw the board
+            // Display game info
+            if (!this.menu) {
+                // render paddle
+                // render each of the other elements
+            } else if (this.menu) {
+                // show the menu otherwise
+            }
+        }
+    }
 }
