7df9483411e2d31bdbc18ccc20bbbce9366375be
kxwzhang <kxwzhang@gmail.com>
2020-09-09 10:33:48 -0700

    Constrain the movement of paddle along the x-axis

--- src/main/paddle.js
+++ src/main/paddle.js
@@ -1,11 +1,24 @@
 class Paddle {
     constructor() {
         this.width = PADDLE.width;
-       
+        this.x = SPACING.left + BOARD.width / 2 - this.width / 2;
+        this.y = GAME_HEIGHT - 3 * BLOCK.height;
+        this.onCD = false;
+        this.cooldown = 30;
+        this.supersized = false;
+        this.beam = false;
+        this.frame;
     }
 
     update() {
         // HANDLE UPDATING STATUS
+        if (keyIsDown(CONTROLS.A)) this.x -= 5; // move left 5
+        if (keyIsDown(CONTROLS.D)) this.x += 5; // move right 5
+        this.x = constrain(
+          this.x,
+          SPACING.left + WALL.left,
+          GAME_WIDTH - SPACING.right - WALL.right - this.width
+        );
     }
 
     show() {
