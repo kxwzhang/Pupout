c3bdf17901cfbb0cc756a1e9aad76dfbd5f81627
kxwzhang <kxwzhang@gmail.com>
2020-09-09 11:06:39 -0700

    Let the offset be equal to 0 on new ball

--- src/main/ball.js
+++ src/main/ball.js
@@ -1,7 +1,23 @@
 class Ball {
   constructor(x, y, dx, dy) {
-      this.x = (BOARD.width/2) + SPACING.left;
-      this.y = GAME_HEIGHT - (3*BLOCK.height) - BALL_RADIUS;
-    
+    this.x = (BOARD.width/2) + SPACING.left;
+    this.y = GAME_HEIGHT - (3*BLOCK.height) - BALL_RADIUS;
+    this.dx = 3;
+    this.dy = 3;
+    // Check if the ball exists on X 
+    if (x) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+    }
+    this.offset = 0;
+    this.moving = false;
+    this.magnet = false;
+  }
+
+  update() {
+    // CHECK FOR COLLISIONS
+    // ?
   }
 }
