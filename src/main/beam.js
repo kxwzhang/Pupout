7fd78201ccfc62ec7ece48fb18b04d0968748e62
kxwzhang <kxwzhang@gmail.com>
2020-09-12 15:25:31 -0700

    Check that beam is destroyed when it's beyond the top of the edges

--- src/main/beam.js
+++ src/main/beam.js
@@ -11,4 +11,17 @@ class Beam {
   show() {
     image(spriteBlueBeam, this.x. this.y, 5, 10);
   }
+
+  hitBlock(block) {
+    if (this.y < block.y + block.height && 
+        this.x < block.x + block.width && 
+        this.x > block.x - 5) {
+      return true;
+    }
+    return false;
+  }
+
+  destroyed() {
+    return (this.y < SPACING.top + WALL.top) ? true : false;
+  }
 }
